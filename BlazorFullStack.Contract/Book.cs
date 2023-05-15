136addd5b709cb163256186fbf0489b412a77f52
ilosvayaron <ilosvayaron@gmail.com>
2023-05-15 20:19:38 +0200

    Add models to external library

---
+++ BlazorFullStack.Contract/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFullStack.Contract
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Publisher { get; set; }
+
+        public int InventoryNumber { get; set; }
+
+        public int PublishYear { get; set; }
+    }
+}
