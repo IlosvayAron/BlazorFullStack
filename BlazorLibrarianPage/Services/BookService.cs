d75802e7aa30cae12ca21a3541cfe17dc558e133
ilosvayaron <ilosvayaron@gmail.com>
2023-05-16 18:03:43 +0200

    Connecting blazor pages to Web API

---
+++ BlazorLibrarianPage/Services/BookService.cs
@@ -0,0 +1,19 @@
+﻿using BlazorFullStack.Contract;
+using System.Net.Http.Json;
+
+namespace BlazorLibrarianPage.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BookService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<IEnumerable<Book>?> GetAllBooksAsync() => _httpClient.GetFromJsonAsync<IEnumerable<Book>>("api/Books");
+
+        public Task<Book?> GetBookByIdAsync(int id) => _httpClient.GetFromJsonAsync<Book?>($"api/Books/{id}");
+    }
+}
---
+++ BlazorLibrarianPage/Services/RentalService.cs
@@ -0,0 +1,19 @@
+﻿using BlazorFullStack.Contract;
+using System.Net.Http.Json;
+
+namespace BlazorLibrarianPage.Services
+{
+    public class RentalService : IRentalService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RentalService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public Task<IEnumerable<Rental>?> GetAllRentalsAsync() => _httpClient.GetFromJsonAsync<IEnumerable<Rental>?>("api/Rentals");
+
+        public Task<Rental?> GetRentalByIdAsync(int id) => _httpClient.GetFromJsonAsync<Rental?>($"api/Rentals/{id}");
+    }
+}
